0fa3ae6b8ccf583a40ba6c27f627443cdf97b142
Nemo <vlado.nemo@gmail.com>
2019-02-13 10:42:13 +0100

    Call tensorboard just once

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ model = Sequential()
 model.add(Dense(2, input_shape=(2,), activation='sigmoid'))
 model.add(Dense(1))
 model.compile(optimizer='sgd', metrics=['acc'], loss='mse')
-epochs = 1000
+epochs = 300
 performance = []
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()), write_graph=True)
 
-for i in range(epochs):
-    model.fit(x=xor_data, y=answers, batch_size=1, epochs=1, verbose=0, callbacks=[tensorboard])
-    # loss, acc = model.evaluate(x=xor_data, y=answers, verbose=0)
-    # performance.append(acc)
+model.fit(x=xor_data, y=answers, batch_size=1, epochs=epochs, verbose=0, callbacks=[tensorboard])
+loss, acc = model.evaluate(x=xor_data, y=answers, verbose=0)
+performance.append(acc)
 
 performance = np.asarray(performance)
 plt.plot(performance)
