55275b0a31f480968684f839b2064d7750dffff9
Nemo <vlado.nemo@gmail.com>
2019-02-13 09:36:55 +0100

    Fit data to the model

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+import numpy as np
 
-xor_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+xor_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+answers = np.array([[0], [1], [1], [0]])
 
 model = Sequential()
 model.add(Dense(2, input_shape=(2,), activation='sigmoid'))
 model.add(Dense(1))
+model.compile(optimizer='sgd', metrics=['acc'], loss='mse')
+model.fit(x=xor_data, y=answers, batch_size=1, epochs=100)
+print(model.predict(xor_data))
 print(model.summary)
