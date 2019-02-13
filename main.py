e4f13a46510db9a5873e93f2eadc7ffe50683125
Nemo <vlado.nemo@gmail.com>
2019-02-13 10:27:14 +0100

    Add TensorBoard

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.python.keras.callbacks import TensorBoard
+from time import time
 
 xor_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 answers = np.array([[0], [1], [1], [0]])
@@ -10,13 +12,14 @@ model = Sequential()
 model.add(Dense(2, input_shape=(2,), activation='sigmoid'))
 model.add(Dense(1))
 model.compile(optimizer='sgd', metrics=['acc'], loss='mse')
-epochs = 300
+epochs = 1000
 performance = []
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()), write_graph=True)
 
 for i in range(epochs):
-    model.fit(x=xor_data, y=answers, batch_size=1, epochs=1, verbose=0)
-    loss, acc = model.evaluate(x=xor_data, y=answers, verbose=0)
-    performance.append(acc)
+    model.fit(x=xor_data, y=answers, batch_size=1, epochs=1, verbose=0, callbacks=[tensorboard])
+    # loss, acc = model.evaluate(x=xor_data, y=answers, verbose=0)
+    # performance.append(acc)
 
 performance = np.asarray(performance)
 plt.plot(performance)
